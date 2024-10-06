343d72bde946a3398bffa5eea05c5cae3a503560
Victor N. Skurikhin <vskurikhin@gmail.com>
2024-10-06 19:17:54 +0300

    Задание 2: Разработка MVP

---
+++ telemetry-microservice/src/test/java/ru/yandex/practicum/smarthome/telemetry/TelemetryApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.yandex.practicum.smarthome.telemetry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelemetryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
